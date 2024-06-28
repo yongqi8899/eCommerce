5c1f30b955ce76b27bec6d497f21f8e0c3d494fd
yongqi8899 <1243994415@qq.com>
2024-06-28 12:53:56 +0200

    fix add key

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,9 @@
 export default function Categories({ categoriesData }) {
   return (
     <div className="flex justify-start flex-wrap gap-6 my-6">
-      {categoriesData.map((category) => {
+      {categoriesData.map((category,index) => {
         return (
-          <button className="btn btn-outline btn-blue" onClick={()=>console.log({category})}>
+          <button className="btn btn-outline btn-blue" key={index} onClick={()=>console.log({category})}>
             {category}
           </button>
         );
